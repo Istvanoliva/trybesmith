049efb14d06b8b667e15cb1d27450a94ef361fd9
Istvan <olivaistvan@gmail.com>
2022-08-11 19:30:44 -0300

    refactor: connection

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,9 +4,9 @@ import mysql2 from 'mysql2/promise';
 dotenv.config();
 
 const connection = mysql2.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 export default connection;
