fc0e9e2197816aeb02ad7cf54d94e0eff65c3556
Istvan <olivaistvan@gmail.com>
2022-08-12 21:37:37 -0300

    feat: (routes) Add route/users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import productsRouter from './products';
+import usersRouter from './users';
 
 const router = express();
 
 router.use('/products', productsRouter);
+
+router.use('/users', usersRouter);
 
 export default router;
