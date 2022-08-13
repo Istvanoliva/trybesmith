cc31d0010838a7461c945764ae8ed64a9a441ad9
Istvan <olivaistvan@gmail.com>
2022-08-12 21:35:19 -0300

    feat: (queries) Add NewUser

--- src/models/queries.ts
+++ src/models/queries.ts
@@ -1,4 +1,5 @@
 export default {
   insert: 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?,?)',
   getAll: 'SELECT * FROM Trybesmith.Products',
+  newUser: 'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)',
 };
