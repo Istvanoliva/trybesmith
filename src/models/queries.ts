38e7d8525789fa3a3fad6d95864b6e39392d0edd
Istvan <olivaistvan@gmail.com>
2022-08-13 14:53:54 -0300

    feat: (queries) Add getOrders

--- src/models/queries.ts
+++ src/models/queries.ts
@@ -2,4 +2,10 @@ export default {
   newProduct: 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?,?)',
   getAllProducts: 'SELECT * FROM Trybesmith.Products',
   newUser: 'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)',
+  getOrders: `
+  SELECT o.id, o.userId, JSON_ARRAYAGG(p.id) AS productsIds
+  FROM Trybesmith.Orders as o
+  INNER JOIN Trybesmith.Products AS p ON o.id = p.orderId
+  GROUP BY o.id
+  ORDER BY o.userId ASC`,
 };
