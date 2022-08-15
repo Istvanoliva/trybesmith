3add3f7ab1a7c5471091f37da0c367dcd241faa9
Istvan <olivaistvan@gmail.com>
2022-08-15 00:56:47 -0300

    feat: (Services/Users) Add findUser() e login()

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,7 +1,10 @@
 import { IUser } from '../interfaces/usersInterface';
+import { ILogin } from '../interfaces/loginInterface';
+
 import connection from '../models/connection';
 import UsersModel from '../models/usersModels';
 import generateToken from '../jwt/tokenGenerator';
+import http from '../middlewares/status';
 
 class UserService {
   model: UsersModel;
@@ -13,6 +16,19 @@ class UserService {
   createUser = async (user: IUser): Promise<string> => {
     const newUser = await this.model.createUser(user);
     const token = generateToken(newUser);
+    return token;
+  };
+
+  findUser = async (user: ILogin): Promise<ILogin> => {
+    const [find] = await this.model.findUser(user);
+    
+    if (!find) throw Object.assign(http.invalidUser);
+    return find;
+  };
+
+  login = async (user: ILogin): Promise<string> => {
+    const find = await this.findUser(user);
+    const token = generateToken(find);
     return token;
   };
 }
