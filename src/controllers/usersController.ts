287ab6179885cbe8262f2a5247271c49e572b806
Istvan <olivaistvan@gmail.com>
2022-08-15 01:00:03 -0300

    feat: (Controllers/Users) Add login()

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -16,6 +16,15 @@ class UsersController {
       next(error);
     }
   };
+
+  login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = await this.service.login(req.body);
+      return res.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default UsersController;
