29484001d4b40d9cbb72c0cd26d73a0c588a951e
Istvan <olivaistvan@gmail.com>
2022-08-15 01:05:23 -0300

    feat: (routes) Cria route/login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import productsRouter from './products';
 import usersRouter from './users';
 import ordersRouter from './orders';
+import loginRouter from './login';
 
 const router = express();
 
@@ -11,5 +12,7 @@ router.use('/products', productsRouter);
 router.use('/users', usersRouter);
 
 router.use('/orders', ordersRouter);
+
+router.use('/login', loginRouter);
 
 export default router;
