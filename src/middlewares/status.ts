a1e41c21ed780aec525831ebcca1e6c65e04bb45
Istvan <olivaistvan@gmail.com>
2022-08-17 18:50:19 -0300

    feat: (status) Add novos status

--- src/middlewares/status.ts
+++ src/middlewares/status.ts
@@ -4,6 +4,8 @@ const http = {
   invalidUser: { status: 401, message: 'Username or password invalid' },
   noName: { status: 400, message: '"name" is required' },
   noAmount: { status: 400, message: '"amount" is required' },
+  noClass: { status: 400, message: '"classe" is required' },
+  noLevel: { status: 400, message: '"level" is required' },
 };
 
 export default http;
