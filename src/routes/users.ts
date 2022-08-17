151dc9ea3420eb941cce160e7eeb8be796239ebf
Istvan <olivaistvan@gmail.com>
2022-08-17 18:54:15 -0300

    feat: (routes/users) Add validacoes na rota

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import UserController from '../controllers/usersController';
+import LoginValidator from '../middlewares/loginValidator';
 
 const usersRouter = Router();
 
 const users = new UserController();
+const validation = new LoginValidator();
 
-usersRouter.post('/', users.createUser);
+usersRouter.post(
+  '/',
+  validation.validateUsername,
+  validation.validatePassword,
+  validation.validateClass,
+  validation.validateLevel,
+  validation.fieldsValidator,
+  users.createUser,
+);
 
 export default usersRouter;
