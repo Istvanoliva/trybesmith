739d681db4877e54d5f80e534475142bade9bc21
Istvan <olivaistvan@gmail.com>
2022-08-22 14:57:02 -0300

    feat: (routes) Add post/orders

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/ordersControllers';
+import orderValidator from '../middlewares/orderValidator';
+import tokenValidator from '../middlewares/tokenMiddleware';
 
 const ordersRouter = Router();
 
 const orders = new OrdersController();
 
 ordersRouter.get('/', orders.getOrders);
+
+ordersRouter.post('/', tokenValidator, orderValidator, orders.create);
 
 export default ordersRouter;
