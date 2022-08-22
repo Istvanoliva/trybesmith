8eb63f8f79aa31ed81c7767c3a2ece05714bf939
Istvan <olivaistvan@gmail.com>
2022-08-22 14:54:20 -0300

    feat: (Controllers/orders) Add create()

--- src/controllers/ordersControllers.ts
+++ src/controllers/ordersControllers.ts
@@ -1,17 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestUser } from '../interfaces/tokenInterface';
+import { IUser } from '../interfaces/usersInterface';
 import OrdersService from '../services/ordersService';
+import ProductService from '../services/productsService';
 
 class OrdersController {
   service: OrdersService;
+
+  productService: ProductService;
   
   constructor() {
     this.service = new OrdersService();
+    this.productService = new ProductService();
   }
 
   getOrders = async (_req: Request, res: Response, next: NextFunction) => {
     try {
       const orders = await this.service.getOrders();
       return res.status(200).json(orders);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  create = async (req: RequestUser, res: Response, next: NextFunction) => {
+    const user = req.user as IUser;
+    const { productsIds } = req.body;
+    try {
+      if (!user.id) throw new Error('Expired token');
+      const orderId = await this.service.create(user.id);
+      await this.productService.update(productsIds, orderId);
+      res.status(201).json({ userId: user.id, productsIds }); 
     } catch (error) {
       next(error);
     }
