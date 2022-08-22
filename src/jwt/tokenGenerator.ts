fd7988e5adef5cb3497eb4aa622abc2b9018b7ff
Istvan <olivaistvan@gmail.com>
2022-08-22 14:46:43 -0300

    feat: (tokenGenerator) verifyToken()

--- src/jwt/tokenGenerator.ts
+++ src/jwt/tokenGenerator.ts
@@ -1,16 +1,13 @@
 import jwt, { Secret, SignOptions } from 'jsonwebtoken';
 import { IUser } from '../interfaces/usersInterface';
 
-const secret: Secret = 'chocolate';
+export const secret: Secret = 'chocolate';
 
 const jwtConfig: SignOptions = {
   expiresIn: '7d',
   algorithm: 'HS256',
 };
 
-const generateToken = (payload: IUser) => {
-  const token = jwt.sign({ payload }, secret, jwtConfig);
-  return token;
-};
+export const generateToken = (payload: IUser) => jwt.sign({ payload }, secret, jwtConfig);
 
-export default generateToken;
+export const verifyToken = (token: string) => jwt.verify(token, secret);
